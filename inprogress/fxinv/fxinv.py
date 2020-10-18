e2d976984ad0919b340d8174a6e01f82b98b681a
Evan Whited <71466144+ewwhited@users.noreply.github.com>
2020-10-18 10:10:43 -0500

    Add files via upload

---
+++ inprogress/fxinv/fxinv.py
@@ -0,0 +1,12 @@
+from manimlib.imports import *
+
+class fxinv(Scene):
+    def construct(self):
+        #Plan:
+        #Recall: Inverse and f'(x)
+        #How to go about solving a diffeq such as f'(x)=f^(-1)(x)?
+        #View through the lens of 'types' of functions? exp, log, poly, trig, etc
+        #In poly case: develop intuition, introduce golden ratio, demonstrate equality
+        #Wrap up?
+        #TBD
+        
